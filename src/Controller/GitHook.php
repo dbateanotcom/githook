6786a8e4a35f3b9eff667349f3dc4372cff73739
Alex Gama <alejandro.gama@gmail.com>
2022-06-20 13:16:48 +0200

    Hook Logic

--- src/Controller/GitHook.php
+++ src/Controller/GitHook.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class GitHook extends AbstractController
 {
@@ -24,12 +24,73 @@ class GitHook extends AbstractController
         LoggerInterface $logger
     ): Response {
 
-        //$_ENV['GITHUB_WEBHOOK_SECRET']
-        $content = \json_decode($requestStack->getCurrentRequest()->getContent(), true);
-        ob_start();
-        var_dump($content);
-        $data = ob_get_clean();
-        $logger->info($data);
-        return new JsonResponse([]);
-   }
+        $post = \json_decode(
+            $requestStack->getCurrentRequest()->getContent(),
+            true
+        );
+
+        if (
+            $post['hook']['config']['sectret']
+            !==
+            $_ENV['GITHUB_WEBHOOK_SECRET']
+        ) {
+        } {
+            throw new UnauthorizedHttpException('Unauthorized');
+        }
+
+        $projects = $this->findGitProjects($_ENV['REPOSITORIES_PATH']);
+
+        foreach ($projects as $p) {
+            $projectConfig = parse_ini_file($p . '/.git/config');
+
+            if (
+                $projectConfig['url'] == $post['repository']['git_url'] ||
+                $projectConfig['url'] == $post['repository']['ssh_url'] ||
+                $projectConfig['url'] == $post['repository']['clone_url']
+            ) {
+                $response = $this->performGitPull($p);
+
+                $logger->info('GitHook: ' . implode("\n", $response));
+            }
+        }
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+
+
+    private function findGitProjects($path)
+    {
+        $iterator = new \DirectoryIterator($path, \RecursiveDirectoryIterator::SKIP_DOTS);
+        foreach ($iterator as $item) {
+            if (
+                $item->isDir() &&
+                $item->getFilename() === '.git' &&
+                file_exists($item->getPathname() . '/config')
+            ) {
+                return [$path];
+            }
+        }
+
+        $paths = [];
+        foreach ($iterator as $item) {
+            if ($item->isDir()) {
+
+                $paths = array_merge(
+                    $paths,
+                    $this->findGitProjects($item->getPathname())
+                );
+            }
+        }
+
+        return $paths;
+    }
+
+
+
+    private function performGitPull($path)
+    {
+        $output = [];
+        exec('git -C ' . $path . ' pull', $output);
+        return $output;
+    }
 }
