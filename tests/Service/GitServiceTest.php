dd70634540f28fb28d28e99c2e50e3c999cdfa1b
Alex Gama <alejandro.gama@gmail.com>
2022-06-27 08:10:54 +0200

    Service Test

--- tests/Service/GitServiceTest.php
+++ tests/Service/GitServiceTest.php
@@ -3,24 +3,30 @@
 namespace App\Tests\Service;
 
 use App\Service\GitService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class GitServiceTest extends \PHPUnit\Framework\TestCase{
-
-    private $service;
-
-    function __construct(GitService $gitService)
-    {
-        $this->service = $gitService;    
-    }
+class GitServiceTest extends KernelTestCase{
 
     public function getPathRepository(){
         return './../../';
     }
-    
+
     /**
      * @depends getPathRepository
      */
     public function testFindRespositories($path){
-        $this->service->findRespositories($path);
+
+        // (1) boot the Symfony kernel
+        self::bootKernel();
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        // (3) run some service & test the result
+        $gitService = $container->get(GitService::class);
+
+        $repos = $gitService->findRespositories($path);
+
+        $this->assertEquals(1, count($repos));
     }
 }
